e0f24a044c13b7def6da18dd24193eb4fc70b0a0
Olivier Henley <olivier.henley@gmail.com>
2025-11-03 21:01:15 -0500

    fix port + pin number for f070rb nucleo

--- src/stm32f070rb_blinky.adb
+++ src/stm32f070rb_blinky.adb
@@ -3,16 +3,16 @@ with STM32F0X0.GPIO; use STM32F0X0.GPIO;
 
 procedure Stm32f070rb_Blinky is
 begin
-   RCC_Periph.AHBENR.IOPBEN := 1;         -- Enable GPIOB clock
-   GPIOB_Periph.MODER.Arr (8) := 2#01#;   -- OUTPUT mode
-   GPIOB_Periph.OTYPER.OT.Arr (8) := 0;   -- Push-pull
-   GPIOB_Periph.PUPDR.Arr (8) := 2#00#;   -- No pull
-   GPIOB_Periph.OSPEEDR.Arr (8) := 2#00#; -- Low speed
-   GPIOB_Periph.BSRR.BR.Arr (8) := 1;     -- Ensure LED is OFF initially
+   RCC_Periph.AHBENR.IOPAEN := 1;         -- Enable GPIOB clock
+   GPIOA_Periph.MODER.Arr (5) := 2#01#;   -- OUTPUT mode
+   GPIOA_Periph.OTYPER.OT.Arr (5) := 0;   -- Push-pull
+   GPIOA_Periph.PUPDR.Arr (5) := 2#00#;   -- No pull
+   GPIOA_Periph.OSPEEDR.Arr (5) := 2#00#; -- Low speed
+   GPIOA_Periph.BSRR.BR.Arr (5) := 1;     -- Ensure LED is OFF initially
    loop
-      GPIOB_Periph.BSRR.BS.Arr (8) := 1;  -- LED ON
+      GPIOA_Periph.BSRR.BS.Arr (5) := 1;  -- LED ON
       delay 1.0;
-      GPIOB_Periph.BSRR.BR.Arr (8) := 1;  -- LED OFF
+      GPIOA_Periph.BSRR.BR.Arr (5) := 1;  -- LED OFF
       delay 1.0;
    end loop;
 end Stm32f070rb_Blinky;
